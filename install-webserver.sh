2f45b11880e981bb5747759d97cd7230c387399d
Malhoura <malhoura@hawk.iit.edu>
2015-11-02 12:34:13 -0600

    edited "php5-mysql"

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql 
 
 git clone https://github.com/Malhoura/itmo-544-444-Application-setup.git
 
@@ -13,7 +13,7 @@ curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 
 sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 
-sudo mv vendor /var/www/html &> tmp/movevendor.txt
+sudo mv vendor /var/www/html &> /tmp/movevendor.txt
 
 sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
 
